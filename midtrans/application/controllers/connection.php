27b3cae7d233909c787599a88468b38893c224da
Samuel Gunawan <samgun9d32@gmail.com>
2022-06-22 18:51:22 +0700

    Change connection for local access

--- midtrans/application/controllers/connection.php
+++ midtrans/application/controllers/connection.php
@@ -1,11 +1,11 @@
 <?php
-    $host = 'localhost';
-    $user = 'adudumas_admin';
-    $password = 'proyekkok2minggu';
-    $database = 'adudumas_db_adudu';
-    $port = '3306';
-    $conn = new mysqli($host, $user, $password, $database);
-    // $conn = new mysqli($host, 'root', '', 'db_adudu');
+    $host = "localhost";
+    $user = "adudumas_admin";
+    $password = "proyekkok2minggu";
+    $database = "adudumas_db_adudu";
+    $port = "3306";
+    // $conn = new mysqli($host, $user, $password, $database);
+    $conn = new mysqli($host, "root", "", "db_adudu");
 
     if($conn -> connect_errno) {
         die("Gagal Connect: " . $conn -> connect_error);
